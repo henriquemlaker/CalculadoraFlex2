b2504d5cee12726fdf2458d40bec9a547e9aba24
Logon Aluno <logonaluno@local.com>
2019-12-14 11:50:17 -0300

    Atualizando

--- app/src/main/java/br/com/henrique/calculadoraflex/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/henrique/calculadoraflex/ui/splash/SplashActivity.kt
@@ -1,12 +1,13 @@
 package br.com.henrique.calculadoraflex.ui.splash
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
 import br.com.henrique.calculadoraflex.R
-import br.com.henrique.calculadoraflex.ui.form.FormActivity
 import br.com.henrique.calculadoraflex.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -17,23 +18,51 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        carregar()
+        val preferences = getSharedPreferences(
+            "user_preferences",
+            Context.MODE_PRIVATE
+        )
+        val isFirstOpen = preferences.getBoolean("open_first", true)
+
+        if (isFirstOpen) {
+            showLogin()
+        } else {
+            markAppAlreadyOpen(preferences)
+            showSplash()
+        }
+
     }
 
-    private fun carregar() {
-        //Carrega a animacao
+    private fun showLogin() {
+        val nextScreen = Intent(this@SplashActivity, LoginActivity::class.java)
+        startActivity(nextScreen)
+        finish()
+    }
+
+
+    private fun markAppAlreadyOpen(preferences: SharedPreferences) {
+        val editor = preferences.edit()
+        editor.putBoolean("open_first", false)
+        editor.apply()
+    }
+
+
+    private fun showSplash() {
         val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
         anim.reset()
         ivLogo.clearAnimation()
-        //Roda a animacao
         ivLogo.startAnimation(anim)
-        //Chama a próxima tela após 3,5 segundos definido na SPLASH_DISPLAY_LENGTH
         Handler().postDelayed({
-            val proximaTela = Intent(this@SplashActivity, LoginActivity::class.java)
-            startActivity(proximaTela)
-            finish()
+            showLogin()
         }, TEMPO_AGUARDO_SPLASHSCREEN)
     }
 }
+
+
+
+
+
+
+
 
 
