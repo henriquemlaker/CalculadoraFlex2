21e563d0ebd60d9011d68b2b23ed31f0117c62b9
Logon Aluno <logonaluno@local.com>
2019-12-14 14:50:17 -0300

    Addicionando fire base

--- app/src/main/java/br/com/henrique/calculadoraflex/ui/login/LoginActivity.kt
+++ app/src/main/java/br/com/henrique/calculadoraflex/ui/login/LoginActivity.kt
@@ -1,13 +1,63 @@
 package br.com.henrique.calculadoraflex.ui.login
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.henrique.calculadoraflex.R
+import br.com.henrique.calculadoraflex.ui.form.FormActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var mAuth: FirebaseAuth
+    private val NEW_USER_REQUEST = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+        if (mAuth.currentUser != null) {
+            goToHome()
+        }
+        btLogin.setOnClickListener {
+            mAuth.signInWithEmailAndPassword(
+                inputLoginEmail.text.toString(),
+                inputLoginPassword.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    goToHome()
+                } else {
+                    Toast.makeText(
+                        this@LoginActivity, it.exception?.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
     }
+
+    private fun goToHome() {
+        val intent = Intent(this, FormActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            NEW_USER_REQUEST ->{
+                inputLoginEmail.setText(data?.getStringExtra("email"))
+            }
+        }
+
+        //if (requestCode == NEW_USER_REQUEST && resultCode == Activity.RESULT_OK) {
+            //inputLoginEmail.setText(data?.getStringExtra("email"))
+        //}
+    }
+
 }
